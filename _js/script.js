6e7257639938bab9fbaf25951fc215ffb919677b
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2023-10-10 11:54:08 -0300

    ajusarado

--- _js/script.js
+++ _js/script.js
@@ -1,30 +1,21 @@
-function abrirImagem(src, alt) {
-    // Cria um novo elemento de imagem
-    var img = document.createElement("img");
-    img.src = src;
-    img.alt = alt;
-    img.style.display = "block";
-    img.style.margin = "0 auto";
+var modal = document.getElementById("modal");
+var images = document.getElementsByClassName("thumbnail");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
 
-    // Cria um novo elemento de div para conter a imagem
-    var div = document.createElement("div");
-    div.id = "imagemAmpliada";
-    div.style.position = "fixed";
-    div.style.top = "0";
-    div.style.left = "0";
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    div.style.display = "flex";
-    div.style.justifyContent = "center";
-    div.style.alignItems = "center";
-    div.appendChild(img);
+for (let i = 0; i < images.length; i++) {
+    let img = images[i];
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        document.body.style.filter = "blur(8px)";
+    }
+}
 
-    // Adiciona o div ao corpo do documento
-    document.body.appendChild(div);
+var span = document.getElementsByClassName("close")[0];
 
-    // Remove o div quando é clicado
-    div.addEventListener("click", function() {
-        document.body.removeChild(div);
-    });
+span.onclick = function() { 
+  modal.style.display = "none";
+  document.body.style.filter = "none";
 }
